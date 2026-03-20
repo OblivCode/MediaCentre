6b6cfc7984083cd0ebfb14d36ac62b744f598b3d
Emmanuel Hamani <emmanuel.s.hamani@gmail.com>
2026-03-20 20:39:06 +0000

    Phase 7: Update MovieCard with posters, tap-to-edit via RatingModal bottom sheet

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/models.dart';
+import '../services/tmdb_service.dart';
 import '../services/volume_manager.dart';
+import '../widgets/rating_modal.dart';
 import 'add_media_screen.dart';
 import 'settings_screen.dart';
 
@@ -100,6 +103,7 @@ class LibraryScreen extends StatelessWidget {
     }
 
     return ListView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 8),
       itemCount: manager.library.children.length,
       itemBuilder: (context, index) {
         final media = manager.library.children[index];
@@ -107,6 +111,7 @@ class LibraryScreen extends StatelessWidget {
           return _MovieCard(
             movie: media,
             onDelete: () => manager.deleteMovie(media.id),
+            onEdit: (updated) => manager.updateMovie(updated),
           );
         }
         return ListTile(
@@ -132,52 +137,114 @@ class LibraryScreen extends StatelessWidget {
 class _MovieCard extends StatelessWidget {
   final MovieBlock movie;
   final VoidCallback onDelete;
+  final ValueChanged<MovieBlock> onEdit;
 
-  const _MovieCard({required this.movie, required this.onDelete});
+  const _MovieCard({
+    required this.movie,
+    required this.onDelete,
+    required this.onEdit,
+  });
+
+  Future<void> _showEditModal(BuildContext context) async {
+    await RatingModal.show(
+      context: context,
+      movie: movie,
+      posterUrl: movie.posterUrl,
+      synopsis: movie.synopsis,
+      runtimeMinutes: movie.runtimeMinutes,
+      onSave: onEdit,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Dismissible(
-        key: Key(movie.id),
-        direction: DismissDirection.endToStart,
-        onDismissed: (_) => onDelete(),
-        background: Container(
-          alignment: Alignment.centerRight,
-          padding: const EdgeInsets.only(right: 20),
+    return Dismissible(
+      key: Key(movie.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) => onDelete(),
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
           color: Colors.red,
-          child: const Icon(Icons.delete, color: Colors.white),
+          borderRadius: BorderRadius.circular(12),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Text(
-                      movie.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  _StarRating(rating: movie.userRating),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Text(
-                '${movie.runtimeMinutes} min',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey[600],
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: InkWell(
+          onTap: () => _showEditModal(context),
+          borderRadius: BorderRadius.circular(12),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: movie.posterUrl != null
+                      ? CachedNetworkImage(
+                          imageUrl: movie.posterUrl!,
+                          width: 70,
+                          height: 105,
+                          fit: BoxFit.cover,
+                          placeholder: (_, __) => Container(
+                            width: 70,
+                            height: 105,
+                            color: Colors.grey[300],
+                            child: const Center(
+                              child: CircularProgressIndicator(strokeWidth: 2),
+                            ),
+                          ),
+                          errorWidget: (_, __, ___) => Container(
+                            width: 70,
+                            height: 105,
+                            color: Colors.grey[300],
+                            child: const Icon(Icons.movie, size: 32),
+                          ),
+                        )
+                      : Container(
+                          width: 70,
+                          height: 105,
+                          color: Colors.grey[300],
+                          child: const Icon(Icons.movie, size: 32),
+                        ),
                 ),
-              ),
-            ],
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 6),
+                      if (movie.runtimeMinutes > 0)
+                        Text(
+                          '${movie.runtimeMinutes} min',
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      const SizedBox(height: 8),
+                      _StarRating(rating: movie.userRating),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey[400],
+                ),
+              ],
+            ),
           ),
         ),
       ),
